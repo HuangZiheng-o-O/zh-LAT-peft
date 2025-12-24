003dd59a8a7c962ab254ef8d7bb81258aa97b74e
huangziheng <2253203008@qq.com>
2025-12-24 00:29:32 -0500

    delete sh

--- mamba-peft/scripts/train/glue130m.sh
+++
@@ -1,3 +0,0 @@
-DEVICES=${1:-0}
-# train
-python run_all.py train.py --device $DEVICES --cfg cfg/exps/glue/*/* --override
--- mamba-peft/scripts/train/spider28b.sh
+++
@@ -1,5 +0,0 @@
-DEVICES=${1:-0}
-# train
-python run_all.py train.py --device $DEVICES --cfg cfg/exps/spider28b/* --override
-# eval
-python run_all.py eval.py --device $DEVICES --eval_cfg cfg/eval/spider28b/* --override
